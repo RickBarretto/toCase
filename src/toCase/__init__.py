087fb743c015b0cde460c00532756e2b723606f7
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2021-04-19 18:14:47 -0300

    Moving the code to toCase folder, and changing ToCase.py to toCase.py

--- src/toCase/__init__.py
+++ src/toCase/__init__.py
@@ -4,6 +4,8 @@ toCase
 toCase is a Case converter made in python, for peoples who wants simplify this feature. It can convert to and from Camel, Pascal, Snake, Kebab and Strings Sentences.
 """
 
+from ToCase import *
+
 __version__ = "0.1.2"
 __author__ = 'Rick Barretto'
 __credits__ = 'Rick Barretto'
